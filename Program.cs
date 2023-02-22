d40de11fe9637c50a69425202cef66b6548e4086
Daniel Mantovani <103224655+dnielMantov@users.noreply.github.com>
2023-02-22 16:36:36 -0300

    Update Program.cs
    Algumas indentações próprias da linguagem com as chaves { }.

--- Program.cs
+++ Program.cs
@@ -6,8 +6,26 @@ namespace csharp_learning
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.ReadLine();
+            int idade = int.Parse(Console.ReadLine());
+
+            if (idade >= 18)
+            {
+                Console.WriteLine("Adulto");
+            } 
+            else if (idade < 18 && idade >= 12) 
+            {
+                Console.WriteLine("Adolescente");
+            }
+             else 
+            {
+                Console.WriteLine("Criança");
+            }
+            /*
+            C# é uma cópia de Java hahah
+            A única coisa que mudaram foi a indentação das chaves,
+            sendo que posso organizá-las da mesma forma que em Java.
+            Mas vou usar conforme a linguagem pede para diferenciar uma da outra.
+            */
         }
     }
 }
