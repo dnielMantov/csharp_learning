43ead863cf9ce336a3096ea25c54cae6df3d0fa1
Daniel Mantovani <103224655+dnielMantov@users.noreply.github.com>
2023-02-23 09:13:12 -0300

    Update TesteEnum.cs

--- TesteEnum.cs
+++ TesteEnum.cs
@@ -1,20 +1,19 @@
 ﻿using System;
 
-namespace csharp_learning
-{
-    class Program
-    {    
-        public static void Main(string[] args)
-        {
-            DiaSemana dia1 = DiaSemana.SEXTA;
-            DiaSemana dia2 = (DiaSemana) 2;
-            DiaSemana dia3 = new DiaSemana();
-            
-            Console.WriteLine(dia1); // SEXTA
+namespace csharp_learning;
 
-            Console.WriteLine(dia2); // QUARTA
+class Program
+{    
+    public static void Main(string[] args)
+    {
+        DiaSemana dia1 = DiaSemana.SEXTA;
+        DiaSemana dia2 = (DiaSemana) 2;
+        DiaSemana dia3 = new DiaSemana();
+        
+        Console.WriteLine(dia1); // SEXTA
 
-            Console.WriteLine(dia3); // SEGUNDA
-        }
+        Console.WriteLine(dia2); // QUARTA
+
+        Console.WriteLine(dia3); // SEGUNDA
     }
 }
